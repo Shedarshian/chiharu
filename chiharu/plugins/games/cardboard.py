071c4c7b3705f33d9217719344e692aa0b62ea59
Shedarshian <qitianyu1998@163.com>
2019-09-22 16:50:21 +0800

    zhu in progress

--- chiharu/plugins/games/cardboard.py
+++ chiharu/plugins/games/cardboard.py
@@ -2,21 +2,83 @@ from abc import ABC, abstractmethod
 from typing import Awaitable
 import asyncio
 import functools
+import random
 
 # example usage for CardGame:
 # Zhu = CardGame()
 #
 # class ZhuBoard(Zhu.Board):
-#     pass
+#     pass # need to implement __init__(), process()
 #
 # class ActionCard(Zhu.Card('Action')):
 #     pass
-#     # can get card deck by deck = ActionCard.Deck
-#     # can use deck.draw(), deck.shuffle(), can set deck.empty = _f(default=shuffle) or more
+#     # can get card deck by deck = ActionCard.deck or Zhu.Deck('Action')
+#     # can use deck.draw(), deck.shuffle(), can set deck.on_empty = _f or more
 #
-# class ZhuPlayer(Zhu.Player):
-#     pass
+# class GuaBiao(ActionCard, name='挂裱', num=1):
+#     pass # need to implement property(description()), usage(), use()
 
+class Deck:
+    def __init__(self, card_cls):
+        self._card_cls = card_cls
+        self._card_list = []
+        self._deck_num = []
+        self._deck = [] # end means the upper most card in deck. saves id.
+        self._discard = []
+    def _add_init_card(self, Card_cls, num):
+        Card_cls._id = len(self._card_list)
+        self._card_list.append(Card_cls)
+        self._deck_num.append(num)
+    def begin(self):
+        for id, num in enumerate(self._deck_num):
+            for i in range(num):
+                self._deck.append(id)
+        random.shuffle(self._deck)
+    @property
+    def is_empty(self):
+        return len(self._deck) == 0
+    def shuffle(self):
+        random.shuffle(self._deck)
+    def collect_discard(self):
+        self._deck.extend(self._discard)
+        self._discard = []
+    def on_empty(self):
+        '''things auto done when empty. default = collect_discard and shuffle.'''
+        #if len(self._discard) == 0:
+        self.collect_discard()
+        self.shuffle()
+    def draw(self, reverse=False):
+        if self.is_empty:
+            self.on_empty()
+            if self.is_empty:
+                # if remain empty after on_empty then return None
+                return None
+        if reverse:
+            return self._card_list[self._deck.pop(0)]
+        else:
+            return self._card_list[self._deck.pop()]
+    def peek(self, reverse=False):
+        if self.is_empty:
+            self.on_empty()
+            if self.is_empty:
+                # if remain empty after on_empty then return None
+                return None
+        if reverse:
+            return self._card_list[self._deck[0]]
+        else:
+            return self._card_list[self._deck[-1]]
+    def discard(self, card):
+        self._discard.append(card._id)
+    def peek_discard(self):
+        if len(self._discard) == 0:
+            return None
+        else:
+            return self._card_list[self._discard[-1]]
+    def put_back(self, card, reverse=False):
+        if reverse:
+            self._deck = [card._id] + self._deck
+        else:
+            self._deck.append(card._id)
 class CardGame:
     def __init__(self):
         class _board(ABC):
@@ -24,11 +86,9 @@ class CardGame:
             def __init__(self, *args, **kwargs):
                 '''need to shuffle decks and something.'''
                 #TODO player assign
-                pass
             @abstractmethod
             async def process(self, *args, **kwargs):
                 '''write it yourself.'''
-                pass
             def next_player(self):
                 pass # TODO default behaviour
         self.Board = _board
@@ -37,11 +97,9 @@ class CardGame:
             @abstractmethod
             def __init__(self, *args, **kwargs):
                 '''need i/o set.'''
-                pass
             @abstractmethod
             async def round(self, *args, **kwargs):
                 '''player's round. generally need to call some other stages.'''
-                pass
             def response(self, name):
                 def _(f: Awaitable):
                     @functools.wraps(f)
@@ -53,16 +111,29 @@ class CardGame:
             async def wait(self, name):
                 return await self.response_dict[name]()
         self.Player = _player
-    def Card(self, name):
+        self._deck = {}
+    def Card(self, deck_name):
         class _card(ABC):
-            #def __init_subclass__(cls,)
+            _game = self
+            deck = self._deck
+            _deck_name = deck_name
+            def __init_subclass__(child, **kwargs):
+                super().__init_subclass__(**kwargs)
+                @classmethod
+                def _(grandchild, num=0):
+                    _card.deck[_card._deck_name]._add_init_card(grandchild, num)
+                child.__init_subclass__ = _
+            name = 'NoName'
+            description = 'NoDescription'
             @abstractmethod
             def usage(self):
                 '''return the args needed to be assigned'''
-                pass
             @abstractmethod
-            def use(self, **kwargs):
+            def use(self, player, **kwargs):
                 '''use the card on kwargs which is given by self.usage()'''
-                pass
+        _deck = Deck(_card)
+        self._deck[deck_name] = _deck
         return _card
+    def Deck(self, name):
+        return self._deck[name]
 
--- chiharu/plugins/games/zhu_core.py
+++ chiharu/plugins/games/zhu_core.py
@@ -3,4 +3,17 @@ from .cardboard import CardGame
 
 Zhu = CardGame()
 
-Zhu.Board
+class ActionCard(Zhu.Card('Action')):
+    pass
+
+class YingLi(ActionCard, num=4):
+    name = '盈利'
+    description = '摸两张行动牌。'
+    def usage(self):
+        return {}
+    def use(self, player):
+        player.draw_action(2)
+
+class ZhuBoard(Zhu.Board):
+    def __init__(self):
+        pass
