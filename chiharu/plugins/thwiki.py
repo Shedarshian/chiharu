342db9b4920263c5c1756c86fae3a6c1c6538366
Shedarshian <qitianyu1998@163.com>
2020-08-24 17:18:09 +0800

    update id logic

--- chiharu/plugins/thwiki.py
+++ chiharu/plugins/thwiki.py
@@ -31,8 +31,8 @@ env_all_can_private.private = True
 config.CommandGroup('thwiki', short_des="THBWiki官方账户直播相关。", des='THBWiki官方账户直播相关。部分指令只可在直播群内使用。', environment=env_all_can_private)
 
 # Version information and changelog
-version = "2.3.10"
-changelog = """2.3.0-10 Changelog:
+version = "2.3.11"
+changelog = """2.3.0-11 Changelog:
 Change:
 -thwiki.grant：推荐需要获得推荐权。获得推荐权的方法是申请加入“THBWiki直播审核群”。并且不需要被推荐人同意。
 若开播后15分钟仍未有人监视，则申请会被自动取消。
