d772fe4dc92702501d2e619c44f1a6ff27b2d510
Shedarshian <qitianyu1998@163.com>
2020-12-03 20:58:11 +0800

    change pool behaviour

--- chiharu/plugins/if.py
+++ chiharu/plugins/if.py
@@ -7,7 +7,7 @@ from .inject import on_command
 config.CommandGroup('if', short_des='魔法禁书目录 幻想收束相关指令。', hide=True)
 
 with open(config.rel('if\\card.json'), encoding='utf-8') as f:
-    premium_card = json.load(f)
+    metainfo, premium_card = json.load(f)
 
 @on_command(('if', 'gacha'), only_to_me=False, hide=True, args=('[pool=0]'))
 @config.ErrorHandle
@@ -16,28 +16,28 @@ async def if_gacha(session: CommandSession):
     使用'-if.gacha 卡池id'抽卡。
     卡池信息请使用'-if.gacha 卡池'或'-if.gacha pool'查询。'''
     if session.current_arg_text in ('卡池', 'pool'):
-        with open(config.rel('if\\pool.json'), encoding='utf-8') as f:
-            pool = json.load(f)
-        await session.send('\n'.join(f'{i}: {l[0]}' for i, l in pool['metainfo'].items()))
+        await session.send('\n'.join(f'{i}: {l[0]}' for i, l in metainfo.items()))
     else:
         pool_id = '0' if session.current_arg_text == '' else session.current_arg_text.strip()
-        with open(config.rel('if\\pool.json'), encoding='utf-8') as f:
-            pool = json.load(f)
-        if pool_id not in pool['metainfo']:
+        if pool_id not in metainfo:
             session.finish('请输入正确的卡池id。')
-        metainfo = pool['metainfo'][pool_id]
-        if metainfo[2] is not None: 
+        pool_metainfo = metainfo[pool_id]
+        if pool_metainfo[2] is not None: 
             pool_id_changed = metainfo[2]
         else:
             pool_id_changed = pool_id
-        l = random.choices(pool[pool_id_changed], [d['weight'] for d in pool[pool_id_changed]], k=10)
-        if metainfo[1].startswith('ex'):
-            l[-1] = random.choices(pool[metainfo[1]], [d['weight'] for d in pool[metainfo[1]]], k=1)[0]
+        with open(config.rel(f'if\\pools\\{pool_id_changed}.json'), encoding='utf-8') as f:
+            pool = json.load(f)
+        l = random.choices(pool, [d['weight'] for d in pool], k=10)
+        if pool_metainfo[1].startswith('ex'):
+            with open(config.rel(f'if\\pools\\{pool_metainfo[1]}.json'), encoding='utf-8') as f:
+                pool1 = json.load(f)
+            l[-1] = random.choices(pool1, [d['weight'] for d in pool1], k=1)[0]
         r = []
         for i, d in enumerate(l):
             if d['type'] == 'premium':
                 a, b = d['card']
-                if metainfo[1] == '2':
+                if pool_metainfo[1] == '2':
                     if i == 9 and b == 0:
                         b = 1
                 r.append(random.choice(premium_card[a][b]))
