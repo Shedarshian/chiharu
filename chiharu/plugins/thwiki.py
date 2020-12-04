19d0828680db4a5ef8ef913135eaf7d289d561a0
Shedarshian <qitianyu1998@163.com>
2020-12-04 19:25:28 +0800

    bugfix

--- chiharu/plugins/thwiki.py
+++ chiharu/plugins/thwiki.py
@@ -1563,7 +1563,7 @@ async def thwiki_blacklist(session: CommandSession):
                 return
             begin += match.span()[1]
             yield int(match.group(1))
-    qqs = list(_tmp(session.current_arg_text))
+    qqs = list(_tmp(session.current_arg))
 
     updated_event = []
     for qq in qqs:
@@ -1939,7 +1939,7 @@ async def _(session: CommandSession):
 async def thwiki_set_alias(session: CommandSession):
     """设置自己显示在直播列表中的别名。只能在直播群使用。
     监视者可@别人设置别人的别名。"""
-    alias, *q = session.current_arg_text.split(' ')
+    alias, *q = session.current_arg.split(' ')
     if_others = False
     if len(q) == 0:
         qq = session.ctx['user_id']
