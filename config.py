d2473e5e61540cfc9f98c20f6df09194b79aa4ee
Shedarshian <qitianyu1998@163.com>
2021-04-25 13:10:34 +0800

    update superuser

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from nonebot.default_config import *
 COMMAND_START = {'-'}
 HOST = '127.0.0.1'
 PORT = 8080
-SUPERUSERS = {1569603950}
+SUPERUSERS = {1569603950, 1440962524}
 NICKNAME = ('千春', '七海千春')
 
 from datetime import timedelta
