741b1acef06db16b52dca92b61b139520b57900a
Shedarshian <qitianyu1998@163.com>
2021-06-22 12:26:06 +0800

    bugfix

--- chiharu/plugins/games/logic_dragon.py
+++ chiharu/plugins/games/logic_dragon.py
@@ -864,7 +864,7 @@ async def dragon_check(session: CommandSession):
             for word in s:
                 if word.fork and len(word.childs) == 1:
                     words.append(s)
-        session.finish("当前活动词为：\n" + '\n'.join(f"{s.word}，{'⚠️' if s.parent is not None and (s.parent.qq == qq or s.parent.parent is not None and s.parent.parent.qq == qq) else ''}id为{s.id_str}" for s in words))
+        session.finish("当前活动词为：\n" + '\n'.join(f"{s.word}，{'⚠️' if s.qq == qq or s.parent is not None and s.parent.qq == qq else ''}id为{s.id_str}" for s in words))
 
 @on_command(('dragon', 'buy'), aliases="购买", only_to_me=False, short_des="购买逻辑接龙相关商品。", args=("id",), environment=env)
 @config.ErrorHandle(config.logger.dragon)
