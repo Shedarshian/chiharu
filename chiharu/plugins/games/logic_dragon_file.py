c27bbd76d4bf5cfe0e0d499ed83ee9a5301b17d6
Shedarshian <qitianyu1998@163.com>
2021-08-12 11:18:50 +0800

    bugfix

--- chiharu/plugins/games/logic_dragon_file.py
+++ chiharu/plugins/games/logic_dragon_file.py
@@ -1812,6 +1812,9 @@ class jiaodai(_card):
                 des = _card.status_dict[c]
                 user.send_char(f"的{des[:des.index('：')]}被取消了！")
                 user.remove_status(c, remove_all=False)
+        if user.qq in global_state['lianhuan']:
+            user.send_char("的铁索连环被取消了！")
+            global_state['lianhuan'].remove(user.qq)
         for c in user.data.daily_status:
             if c in _card.debuffs and has > 0:
                 has -= 1
