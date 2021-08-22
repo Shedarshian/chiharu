d1539b764577bea86d9191f12ed18367809d9c4f
Shedarshian <qitianyu1998@163.com>
2021-08-22 17:09:20 +0800

    bugfix

--- chiharu/plugins/games/logic_dragon.py
+++ chiharu/plugins/games/logic_dragon.py
@@ -615,7 +615,7 @@ async def dragon_construct(buf: SessionBuffer):
                     for i in range(n):
                         if random.random() > 0.9:
                             buf.send("你获得了一张【吸血鬼】！")
-                            user.draw(0, cards=[Card(-2)])
+                            await user.draw(0, cards=[Card(-2)])
                 if j := user.check_status('j') and not me.check_daily_status('i') and random.random() > 0.95:
                     user.send_log("的玩偶匣爆炸了！")
                     qqs = {user.qq}
