76e4b1709b8c14a44f7bfdd4454f97f58648255d
Shedarshian <qitianyu1998@163.com>
2021-10-02 17:16:58 +0800

    bugfix

--- chiharu/plugins/games/logic_dragon.py
+++ chiharu/plugins/games/logic_dragon.py
@@ -635,7 +635,7 @@ async def dragon_construct(buf: SessionBuffer):
                         n, nb, na1, na2 = _(nb, user.check_status('a'), user.check_status('A'))
                         if n:
                             buf.send("你从五个人前面接来了判决β！")
-                            uawait ser.kill()
+                            await user.kill()
                             user.remove_status('a')
                             user.remove_status('A')
                             user.add_status('a' * na1 + 'A' * na2)
