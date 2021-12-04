20bc1a63c7317954b595a85d5e77d57229cae033
Shedarshian <qitianyu1998@163.com>
2021-12-04 12:30:14 +0800

    new user bug fixed

--- chiharu/plugins/games/logic_dragon_file.py
+++ chiharu/plugins/games/logic_dragon_file.py
@@ -33,7 +33,7 @@ def find_or_new(qq: int):
         config.logger.dragon << "【WARNING】试图find qq=0的node。"
     t = config.userdata.execute("select * from dragon_data where qq=?", (qq,)).fetchone()
     if t is None:
-        extra_data_init = me.extra.data
+        extra_data_init = me.extra.data.pack()
         config.userdata.execute('insert into dragon_data (qq, jibi, draw_time, today_jibi, today_keyword_jibi, death_time, card, status, daily_status, status_time, card_limit, shop_drawn_card, event_pt, spend_shop, equipment, event_stage, event_shop, extra_data, dead) values (?, 0, 0, 10, 10, ?, ?, ?, ?, ?, 4, 1, 0, 0, ?, 0, 0, ?, false)', (qq, '', '', '', '', '[]', '{}', extra_data_init))
         t = config.userdata.execute("select * from dragon_data where qq=?", (qq,)).fetchone()
     return t
@@ -464,6 +464,8 @@ class ExtraData:
     assembling: int # unsigned long
     hp: int         # unsigned int
     mp: int         # unsigned int
+    def pack(self):
+        return pack(extra_data_format, *astuple(self))
     @classmethod
     def make(cls, data, save):
         return cls(*unpack(extra_data_format, data), lambda self: save(pack(extra_data_format, *self)))
