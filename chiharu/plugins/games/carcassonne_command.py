abc5a8a83ea32a97f52ab71bd1c60f49e2af9e41
Shedarshian <qitianyu1998@163.com>
2023-05-29 12:20:47 +0800

    fix

--- chiharu/plugins/games/carcassonne_command.py
+++ chiharu/plugins/games/carcassonne_command.py
@@ -540,10 +540,10 @@ async def ccs_rule(session: CommandSession):
 packs = readPackData()["packs"]
 ccs_rule.__doc__ = "查看卡卡颂规则（*为包含起始板块）。\n" + \
     '\n'.join((f"ex{pack['id']}. " + pack.get("full_name", pack["name"]) + "\n    " +
-        '；'.join(f"({chr(ord('a') + i)}) {name} " for i, name in enumerate(pack["things"]) if i not in pack.get("undone", [])) + '。')
+        '；'.join(f"({chr(ord('a') + i)}) {name}" for i, name in enumerate(pack["things"]) if i not in pack.get("undone", [])) + '。')
         for pack in packs if "things" in pack)
 del packs
-on_command(('cacason', 'rule'), only_to_me=False, short_des="查询卡卡颂扩展列表与扩展规则。", args=("ex?",))(ccs_rule)
+on_command(('cacason', 'rule'), only_to_me=False, short_des="查询卡卡颂扩展列表与扩展规则。", args=("[ex?]",))(ccs_rule)
 
 @on_command(('cacason', 'check'), only_to_me=False, display_id=998)
 @config.ErrorHandle
