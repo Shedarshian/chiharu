5cf40b4f159693af6c6dd5f90c478f8291d1f9e4
Shedarshian <qitianyu1998@163.com>
2024-05-07 22:14:27 +0800

    ok

--- bot-nb2-test.py
+++ bot-nb2-test.py
@@ -1,18 +1,18 @@
 import nonebot
 from nonebot.adapters.console import Adapter as ConsoleAdapter  # 避免重复命名
 
-# 初始化 NoneBot
-nonebot.init()
-
-# 注册适配器
-driver = nonebot.get_driver()
-driver.config.log_level = "TRACE"
-driver.register_adapter(ConsoleAdapter)
-
-# 在这里加载插件
-# nonebot.load_builtin_plugins("echo")  # 内置插件
-# nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
-nonebot.load_plugins("chiharu2/plugins")  # 本地插件
-
 if __name__ == "__main__":
+    # 初始化 NoneBot
+    nonebot.init()
+
+    # 注册适配器
+    driver = nonebot.get_driver()
+    driver.config.log_level = "TRACE"
+    driver.register_adapter(ConsoleAdapter)
+
+    # 在这里加载插件
+    # nonebot.load_builtin_plugins("echo")  # 内置插件
+    # nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
+    nonebot.load_plugins("chiharu2/plugins")  # 本地插件
+
     nonebot.run()
--- bot-nb2.py
+++ bot-nb2.py
@@ -2,18 +2,18 @@ import nonebot
 from nonebot.adapters.discord import Adapter as DiscordAdapter
 from nonebot.adapters.console import Adapter as ConsoleAdapter  # 避免重复命名
 
-# 初始化 NoneBot
-nonebot.init()
-
-# 注册适配器
-driver = nonebot.get_driver()
-# driver.register_adapter(ConsoleAdapter)
-driver.register_adapter(DiscordAdapter)
-
-# 在这里加载插件
-# nonebot.load_builtin_plugins("echo")  # 内置插件
-# nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
-nonebot.load_plugins("chiharu2/plugins")  # 本地插件
-
 if __name__ == "__main__":
+    # 初始化 NoneBot
+    nonebot.init()
+
+    # 注册适配器
+    driver = nonebot.get_driver()
+    # driver.register_adapter(ConsoleAdapter)
+    driver.register_adapter(DiscordAdapter)
+
+    # 在这里加载插件
+    # nonebot.load_builtin_plugins("echo")  # 内置插件
+    # nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
+    nonebot.load_plugins("chiharu2/plugins")  # 本地插件
+
     nonebot.run()
