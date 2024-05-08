e26efc3662c6eb8f87a2e59bba36f8008a4c96db
Shedarshian <qitianyu1998@163.com>
2024-05-08 21:19:11 +0800

    fix

--- chiharu2/plugins/config.py
+++ chiharu2/plugins/config.py
@@ -8,7 +8,7 @@ from os import path
 import traceback
 from nonebot.message import run_postprocessor
 from nonebot.matcher import Matcher
-from nonebot.adapters.discord import Bot, MessageEvent, MessageSegment
+from nonebot.adapters.discord import Bot, Event, InteractionCreateEvent, MessageSegment, MessageEvent
 
 PATH = "C:\\coolq_data\\"
 PATH_IMG = "C:\\go\\data\\images"
@@ -28,7 +28,14 @@ selfqq = 2711644761
 is_chinatsu = True
 
 @run_postprocessor
-async def _(bot: Bot, event: MessageEvent, e: Exception):
-    channel = MessageSegment.mention_channel(event.channel_id)
-    user = MessageSegment.mention_user(event.user_id)
-    await bot.send_to(1237726203029225484, "A Error occured in channel: " + channel + " by user " + user + ".\n The stack trace is:\n" + traceback.format_exc())
+async def _(bot: Bot, event: Event, e: Exception):
+    if isinstance(event, InteractionCreateEvent):
+        channel = MessageSegment.mention_channel(event.channel_id) if event.channel_id else "<no id>"
+        user = MessageSegment.mention_user(event.member.user.id) if event.member and event.member.user else "<no id>"
+        await bot.send_to(1237726203029225484, "An Error occured in channel: " + channel + " by user " + user + ".\n The stack trace is:\n" + traceback.format_exc())
+    elif isinstance(event, MessageEvent):
+        channel = MessageSegment.mention_channel(event.channel_id)
+        user = MessageSegment.mention_user(event.user_id)
+        await bot.send_to(1237726203029225484, "An Error occured in channel: " + channel + " by user " + user + ".\n The stack trace is:\n" + traceback.format_exc())
+    else:
+        await bot.send_to(1237726203029225484, "An Error occured.\n The stack trace is:\n" + traceback.format_exc())
