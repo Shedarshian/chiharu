9e13c17308eae5d932e355f7625993ae614377e7
Shedarshian <qitianyu1998@163.com>
2024-05-10 10:59:53 +0800

    ee

--- loop.bat
+++ loop.bat
@@ -1,5 +1,5 @@
 :label
-"C:\Program Files\proxychains_0.6.8_win32_x64\proxychain.exe" python ./bot-nb2.py
+start "" "C:\Program Files\proxychains_0.6.8_win32_x64\proxychain.exe" python ./bot-nb2.py
 echo "restarting in 5 seconds"
 timeout /T 5
 goto label
