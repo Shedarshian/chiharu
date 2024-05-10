6fd81e84dda9891af9220afd52cca2cfc028b147
Shedarshian <qitianyu1998@163.com>
2024-05-10 18:49:44 +0800

    begin

--- chiharu2/plugins/game.py
+++ chiharu2/plugins/game.py
@@ -1,12 +1,12 @@
 from typing import Tuple, Any, NoReturn
 from collections.abc import Coroutine
+from functools import wraps
 import json
 from nonebot.dependencies import Param
 from nonebot.params import Depends
 from nonebot.params import CommandArg
-from nonebot.matcher import Matcher
 from nonebot import on_message, on_notice
-from nonebot.adapters.discord import on_slash_command, Event, Bot, Message, MessageSegment
+from nonebot.adapters.discord import on_slash_command, Event, Bot, Message, MessageSegment, ReadyEvent
 from nonebot.adapters.discord.api import SubCommandOption, SubCommandGroupOption, Interaction, Button, ButtonStyle, ActionRow, StringOption
 from nonebot.adapters.discord.event import MessageComponentInteractionEvent
 from .helper.helper import rel, getGroup, getUser, Group, DiscordGroup, User, DiscordUser
@@ -52,6 +52,8 @@ matcher = on_slash_command(name="play",
 
 GameData = dict[str, Any]
 DeleteFunc = Coroutine[Any, Any, NoReturn]
+matcher_message = on_message()
+click = on_notice()
 
 class GameSameGroup:
     all_games: 'dict[str, GameSameGroup]' = {}
@@ -64,21 +66,17 @@ class GameSameGroup:
         self.name_zh = name_zh
         self.begin_player = player
 
-    async def get_event_data(self, event: Event, matcher: Matcher):
-        if channel := await getGroup(event, matcher):
-            data = self.uncomplete.get(channel) or self.center.get(channel)
-            if data and data['game'].name == self.name:
-                return data
-        return None
+    async def get_event_data(self, channel: DiscordGroup=Depends(getGroup)):
+        data = self.uncomplete.get(channel) or self.center.get(channel)
+        if data and data['game'] is self:
+            return data
     @property
     def data(self):
         return Depends(self.get_event_data)
-    async def get_delete_func(self, event: Event, matcher: Matcher):
-        if channel := await getGroup(event, matcher):
-            async def _h():
-                self.center.pop(channel)
-            return _h
-        return None
+    async def get_delete_func(self, channel: DiscordGroup=Depends(getGroup)):
+        async def _h():
+            self.center.pop(channel)
+        return _h
     @property
     def delete_func(self):
         return Depends(self.get_delete_func)
@@ -89,11 +87,11 @@ class GameSameGroup:
             return
         if user not in data['players']:
             return
-    @property
-    def check_game(self):
-        pass
+    async def checkInGroup(self, group: DiscordGroup=Depends(getGroup)):
+        if group not in self.center or self.center[group]["game"] is not self:
+            await matcher_message.finish()
 
-    async def checkBegin(self, group: DiscordGroup, bot: Bot) -> bool:
+    async def checkToBegin(self, group: DiscordGroup, bot: Bot) -> bool:
         "True表示成功开始游戏"
         if group not in self.uncomplete:
             return False
@@ -104,21 +102,22 @@ class GameSameGroup:
             if n > self.begin_player[1]:
                 await bot.send_to(group.channel_id, "因同步原因，参与游戏人数已超过上限，最后点击按钮的玩家无法参与对局。")
             self.uncomplete[group]["players"] = self.uncomplete[group]["players"][:self.begin_player[1]]
-            await self.begin(group)
+            self.uncomplete[group]["toBegin"] = True
             return True
         return False
-    async def begin(self, group: DiscordGroup):
-        if group not in self.uncomplete:
-            return False
-        if self.uncomplete[group]["game"] is not self:
-            return False
-        data = self.uncomplete.pop(group)
-        self.center[group] = data
-        # 执行效果
-        pass
-    def process(self):
-        @matcher.handle_sub_command('play', self.name, 'begin')
-        async def begin(bot: Bot, group: DiscordGroup=Depends(getGroup), user: User=Depends(getUser)):
+    def checkBegin(self):
+        for group, data in self.uncomplete.items():
+            if data["game"] is not self:
+                continue
+            if data.get("toBegin", False):
+                data.pop("toBegin")
+                self.uncomplete.pop(group)
+                self.center[group] = data
+                return True
+        return False
+    def begin(self):
+        from pydantic import Field
+        async def onBegin(bot: Bot, group: DiscordGroup=Depends(getGroup), user: User=Depends(getUser)):
             if group in self.uncomplete:
                 await matcher.send_response("本群已有对局邀请！")
                 return
@@ -126,7 +125,7 @@ class GameSameGroup:
                 await matcher.send_response("本群已有对局！")
                 return
             self.uncomplete[group] = {"player": [user], "game": self}
-            if not await self.checkBegin(group, bot):
+            if not await self.checkToBegin(group, bot):
                 labels = f"## {self.name_zh}游戏对局\n"
                 if self.begin_player[0] == self.begin_player[1]:
                     labels += f"游戏人数：{self.begin_player[0]}"
@@ -146,39 +145,15 @@ class GameSameGroup:
                 await matcher.send_response(labels + buttons)
                 msg = await matcher.get_response()
                 self.uncomplete[group]["message_id"] = msg.id
+        f1 = matcher.handle_sub_command('play', self.name, 'begin',
+                parameterless=[Depends(onBegin), Depends(self.checkBegin, validate=Field(default=True, const=True))])
+        f2 = click.handle([Depends(checkClick, validate=Field(default=True, const=True)),
+                Depends(self.checkBegin, validate=Field(default=True, const=True))])
+        return lambda f: f2(f1(f))
 
-        click = on_notice()
-        @click.handle()
-        async def checkClick(bot: Bot, event: MessageComponentInteractionEvent, group: DiscordGroup=Depends(getGroup), user: DiscordUser=Depends(getUser)):
-            if group not in self.uncomplete:
-                return
-            if event.message.id != self.uncomplete[group]["message_id"]:
-                return
-            button = event.data.custom_id
-            if button == "attend":
-                if user in self.uncomplete[group]["player"]:
-                    await click.send(MessageSegment.mention_user(user.user_id) + "已在对局中！")
-                    return
-                self.uncomplete[group]["player"].append(user)
-                message_id = self.uncomplete[group]["message_id"]
-                await click.send(MessageSegment.mention_user(user.user_id) + "已成功加入对局！")
-                if await self.checkBegin(group, bot):
-                    await bot.delete_message(channel_id=group.channel_id, message_id=message_id)
-            elif button == "begin":
-                if user not in self.uncomplete[group]["player"]:
-                    await click.send(MessageSegment.mention_user(user.user_id) + "不在对局中，无法启动游戏！")
-                    return
-                if len(self.uncomplete[group]["players"]) < self.begin_player[0]:
-                    await click.send("匹配人数未达下限，请耐心等待！")
-                    return
-                message_id = self.uncomplete[group]["message_id"]
-                await bot.delete_message(channel_id=group.channel_id, message_id=message_id)
-                await self.begin(group)
-            elif button == "close":
-                pass
-
+    def process(self):
         @matcher.handle_sub_command('play', self.name, 'end')
-        async def play_end(bot: Bot, event: Interaction, group: Group | None=Depends(self.get_group)):
+        async def play_end(bot: Bot, event: Interaction, group: Group | None=Depends(getGroup)):
             if group is None or not event.guild_id or not event.user or not event.user.id or not event.channel_id:
                 await matcher.send_response("无法结束！")
                 return
@@ -188,10 +163,9 @@ class GameSameGroup:
             if not member.permissions:
                 await matcher.send_response("无法结束！")
                 return
-            member.permissions & (1 << 3)
+            # member.permissions & (1 << 3)
 
-        matcher_message = on_message()
-        return matcher_message.handle()
+        return matcher_message.handle([Depends(self.checkInGroup)])
     def open_data(self, qq):
         try:
             with open(rel(f'games\\user_data\\{qq}.json'), encoding='utf-8') as f:
@@ -211,4 +185,34 @@ class GameSameGroup:
         with open(rel(f'games\\user_data\\{qq}.json'), 'w', encoding='utf-8') as f:
             f.write(json.dumps(data, ensure_ascii=False,
                                indent=4, separators=(',', ': ')))
+
+async def checkClick(bot: Bot, event: MessageComponentInteractionEvent, group: DiscordGroup=Depends(getGroup), user: DiscordUser=Depends(getUser)):
+    if group not in GameSameGroup.uncomplete:
+        return False
+    if event.message.id != GameSameGroup.uncomplete[group]["message_id"]:
+        return False
+    button = event.data.custom_id
+    game: GameSameGroup = GameSameGroup.uncomplete[group]["game"]
+    if button == "attend":
+        if user in GameSameGroup.uncomplete[group]["player"]:
+            await click.send(MessageSegment.mention_user(user.user_id) + "已在对局中！")
+            return False
+        GameSameGroup.uncomplete[group]["player"].append(user)
+        message_id = GameSameGroup.uncomplete[group]["message_id"]
+        await click.send(MessageSegment.mention_user(user.user_id) + "已成功加入对局！")
+        if await game.checkToBegin(group, bot):
+            await bot.delete_message(channel_id=group.channel_id, message_id=message_id)
+    elif button == "begin":
+        if user not in GameSameGroup.uncomplete[group]["player"]:
+            await click.send(MessageSegment.mention_user(user.user_id) + "不在对局中，无法启动游戏！")
+            return False
+        if len(GameSameGroup.uncomplete[group]["players"]) < game.begin_player[0]:
+            await click.send("匹配人数未达下限，请耐心等待！")
+            return False
+        message_id = GameSameGroup.uncomplete[group]["message_id"]
+        await bot.delete_message(channel_id=group.channel_id, message_id=message_id)
+        GameSameGroup.uncomplete[group]["toBegin"] = True
+        return True
+    elif button == "close":
+        pass
 
