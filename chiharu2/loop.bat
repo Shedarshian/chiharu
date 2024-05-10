d66de1f88ab0e11448862fbe5bc4a63fff391459
Shedarshian <qitianyu1998@163.com>
2024-05-10 10:38:35 +0800

    loop

---
+++ chiharu2/loop.bat
@@ -0,0 +1,13 @@
+:label
+proxychain python ./bot-nb2.py
+echo "restarting in 5"
+timeout /T 1
+echo "restarting in 4"
+timeout /T 1
+echo "restarting in 3"
+timeout /T 1
+echo "restarting in 2"
+timeout /T 1
+echo "restarting in 1"
+timeout /T 1
+Go back to label
