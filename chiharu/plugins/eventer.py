dd401fa543accb246fa8bc438fae734e7e59485b
Shedarshian <qitianyu1998@163.com>
2024-05-14 21:48:54 +0800

    remove not needed in chiharu1

--- chiharu/plugins/eventer.py
+++
@@ -1,4 +0,0 @@
-from . import config
-from .inject import on_command
-from nonebot import CommandSession, permission
-
--- chiharu/plugins/games/chiharu.py
+++
@@ -1,5 +0,0 @@
-
-from .. import config, game
-from ..config import on_command
-from nonebot import CommandSession
-
