69ae0b1b71d33af07ec50632dfb84d07385148f1
Shedarshian <qitianyu1998@163.com>
2024-05-19 15:57:25 +0800

    e

--- chiharu2/plugins/helper/break_xurui.py
+++ chiharu2/plugins/helper/break_xurui.py
@@ -2,12 +2,21 @@ from typing import List, Tuple, Optional, Union
 from functools import total_ordering
 from copy import deepcopy
 from numbers import Integral
+import math
 import operator
 import itertools
 
 class ENumber:
-    def __init__(self, num: float, exp: int) -> None:
-        self.num = num
-        self.exp = exp
-    def check(self):
+    Tolerance = 1e-18
+    MaxSignificantDigits = 17
+    ExpLimit = 2 ** 128 - 1
+    def __init__(self, mantissa: float, exponent: int, normalize=True) -> None:
+        self.mantissa = mantissa
+        self.exponent = exponent
+        if normalize:
+            self.normalize()
+    def normalize(self):
+        if 1 <= self.mantissa < 10 or not math.isfinite(self.mantissa):
+            return
+        tempExp = math.floor(math.log10(abs(self.mantissa)))
         pass
