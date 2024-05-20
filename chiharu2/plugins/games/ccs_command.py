75c3ec73354a58af953bd5ac4c5369d45adc37de
Shedarshian <qitianyu1998@163.com>
2024-05-20 19:48:41 +0800

    bugfix

--- chiharu2/plugins/games/ccs_command.py
+++ chiharu2/plugins/games/ccs_command.py
@@ -138,9 +138,9 @@ async def ccs_process(matcher: Matcher, state: T_State,
     if data.get('waiting_player_num'):
         if command in "23456":
             # 开始游戏
-            extensions = state.get("extensions", {})
+            extensions = data.get("extensions", {})
             extensions[0] = "a"
-            board: Board = Board(extensions, [data['players'][0]] * int(command), state.get("starting_tile", 0), group.channel_id)
+            board: Board = Board(extensions, [data['players'][0]] * int(command), data.get("starting_tile", 0), group.channel_id)
             data['board'] = board
             await board.advance(send, delete_func)
             data['waiting_player_num'] = False
