09ae7e50504c2669b6383b8c3e6f9955866a322e
Shedarshian <qitianyu1998@163.com>
2024-05-20 19:03:23 +0800

    save error information

--- chiharu2/plugins/config.py
+++ chiharu2/plugins/config.py
@@ -14,10 +14,15 @@ async def _(bot: Bot, event: Event, e: Exception):
     if isinstance(event, InteractionCreateEvent):
         channel = MessageSegment.mention_channel(event.channel_id) if event.channel_id else "<no id>"
         user = MessageSegment.mention_user(event.member.user.id) if event.member and event.member.user else "<no id>"
-        await bot.send_to(1237726203029225484, "An Error occured in channel: " + channel + " by user " + user + ".\n The stack trace is:\n```\n" + ''.join(traceback.format_exception(e)) + "```")
+        msg = "An Error occured in channel: " + channel + " by user " + user + ".\n The stack trace is:\n```\n" + ''.join(traceback.format_exception(e)) + "```" # type: ignore
     elif isinstance(event, MessageEvent):
         channel = MessageSegment.mention_channel(event.channel_id)
         user = MessageSegment.mention_user(event.user_id)
-        await bot.send_to(1237726203029225484, "An Error occured in channel: " + channel + " by user " + user + ".\n The stack trace is:\n```\n" + ''.join(traceback.format_exception(e)) + "```")
+        msg = "An Error occured in channel: " + channel + " by user " + user + ".\n The stack trace is:\n```\n" + ''.join(traceback.format_exception(e)) + "```" # type: ignore
     else:
-        await bot.send_to(1237726203029225484, "An Error occured.\n The stack trace is:\n```\n" + ''.join(traceback.format_exception(e)) + "```")
+        msg = "An Error occured.\n The stack trace is:\n```\n" + ''.join(traceback.format_exception(e)) + "```"
+    from .helper.helper import rel
+    import datetime
+    with open(rel("error.txt"), '+') as f:
+        f.write(datetime.datetime.now().isoformat() + msg + "\n\n")
+    await bot.send_to(1237726203029225484, msg)
