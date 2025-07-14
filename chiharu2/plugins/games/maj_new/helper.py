f63871576e4f32221bc351d8d257d43a057654e0
shedarshian <shedarshian@gmail.com>
2025-07-14 20:53:07 +0900

    config first version

--- chiharu2/plugins/games/maj_new/helper.py
+++ chiharu2/plugins/games/maj_new/helper.py
@@ -1,6 +1,70 @@
 from abc import ABC
 from dataclasses import dataclass
-from typing import Generator, TypeVar
+from typing import Generator, TypeVar, Literal
+from enum import IntFlag
+
+@dataclass
+class HaiConfig:
+    hasHua: bool = False            # 是否有花牌
+    hasFeng: bool = True            # 是否有风牌
+    hasSanYuan: bool = True         # 是否有三元牌
+    wangPai: int = 14               # 王牌数量
+    shoupaiNum: int = 13            # 手牌数量
+    hasBaopai: bool = False         # 是否有宝牌
+    hasLibaopai: bool = False       # 是否有里宝牌
+    hasHongbaopai: bool = False     # 是否有红宝牌
+@dataclass
+class RuleConfig:
+    class BanMingpai(IntFlag):
+        none = 0
+        chi = 1 << 0
+        peng = 1 << 1
+        minggang = 1 << 2
+        angang = 1 << 3
+        jiagang = 1 << 4
+    banMingpai: BanMingpai = 0          # 禁止吃碰杠
+    banShiti: bool = False              # 是否禁止食替
+    shepaizhenting: bool = False        # 是否存在舍牌振听
+    lizhizhenting: bool = False         # 是否存在立直振听
+    tongxunzhenting: bool = False       # 是否存在同巡振听
+    genzhangmianze: bool = False        # 是否存在跟张免责
+    class Tuzhongliuju(IntFlag):
+        none = 0
+        jiuzhongjiupai = 1 << 0
+        sifenglianda = 1 << 1
+        sigangsanliao = 1 << 2
+        sijializhi = 1 << 3
+        sanjiaheliao = 1 << 4
+    tuzhongiuju: Tuzhongliuju = Tuzhongliuju.none
+    liujumanguan: bool = False          # 是否存在流局满贯
+    angangmingpai: bool = True          # 暗杠是否明牌
+    hasQianggang: bool = True           # 是否允许抢杠
+    guoshiwushuangQiangangang: bool = False     # 是否允许国士无双抢暗杠
+    minggangbaopai: bool = False        # 明杠是否包牌
+    fufenxuxing: bool = True            # 是否允许负分续行
+@dataclass
+class ProcedureConfig:
+    playerNum: Literal[3, 4] = 4    # 玩家数量，3人麻将时自动去除北家
+    baNum: Literal[1, 2, 4] = 2     # 场数量，1为东风场，2为半庄，4为全庄
+    renChan: bool = True            # 是否允许连庄
+@dataclass
+class SpecialTypeConfig:
+    fafu: int = 0                   # 流局罚符
+    shiduan: bool = False           # 是否允许食断
+    xiru: bool = False              # 是否允许西入
+    beiru: bool = False             # 是否允许北入
+    kongtingfafu: bool = False      # 空听（听的所有牌都在自己手中）是否罚符
+    alllastLianzhuang: bool = False # 是否允许all last连庄
+    toutiao: bool = False           # 是否存在头跳
+    fanfu: bool = 1                 # 番缚
+    gangbaopaijifan: bool = False   # （非暗杠的）杠宝牌是否立即翻开
+    leijiyiman: bool = False        # 是否存在累计役满
+
+@dataclass
+class Config:
+    hai: HaiConfig = HaiConfig()
+    rule: RuleConfig = RuleConfig()
+    procedure: ProcedureConfig = ProcedureConfig()
 
 @dataclass
 class Send:
--- chiharu2/plugins/games/maj_new/maj.py
+++ chiharu2/plugins/games/maj_new/maj.py
@@ -1,59 +1,59 @@
 import random
 from enum import Enum, auto
 from typing import TypeVar, Generic, TYPE_CHECKING
-from .helper import Send, Recieve, TAsync
+from .helper import Send, Recieve, TAsync, Config
 
 class Color(Enum):
     z = auto() # 只有字牌不能形成顺子
     m = auto()
     p = auto()
     s = auto()
+    h = auto() # 花牌
 class FuuroType(Enum):
     chi = auto()
-    pon = auto()
-    minkan = auto()
-    ankan = auto()
-    kakan = auto()
-    pei = auto()
+    peng = auto()
+    minggang = auto()
+    angang = auto()
+    jiagang = auto()
+    babei = auto()
+    buhua = auto()
 
-THai = TypeVar("THai", bound="BaseHai")
-TPlayer = TypeVar("TPlayer", bound="BasePlayer")
-TBoard = TypeVar('TTable', bound='BaseBoard')
-class BaseHai:
+class Pai:
     def __init__(self, color: Color, num: int):
         self.color = color
         self.num = num
-    def __eq__(self, value: 'BaseHai') -> bool:
+    def __eq__(self, value: 'Pai') -> bool:
         return self.color == value.color and self.num == value.num
-    def __lt__(self, value: 'BaseHai') -> bool:
+    def __lt__(self, value: 'Pai') -> bool:
         if self.color != value.color:
             return self.color.value < value.color.value
         return self.num < value.num
     def __hash__(self) -> int:
         return hash((self.color, self.num))
 
-class Fuuro(Generic[THai]):
-    def __init__(self, type: FuuroType, hai: tuple[THai], fromPlayerPos: int = -1):
+class Fulu:
+    def __init__(self, type: FuuroType, hai: tuple[Pai], fromPlayerPos: int = -1):
         self.type = type
         self.hai = hai
         self.fromPlayerPos: int = fromPlayerPos
 
-class BasePlayer(Generic[THai, TBoard]):
-    def __init__(self, board: TBoard, pos: int):
+class Player:
+    def __init__(self, board: 'Board', pos: int):
         self.board = board
         self.pos = pos
-        self.tensu: int = 0
-        self.tehai: list[THai] = []
-        self.fuuro: list[Fuuro[THai]] = []
-        self.haiho: list[THai] = []
+        self.point: int = 0
+        self.shoupai: list[Pai] = []
+        self.fulu: list[Fulu] = []
+        self.paihe: list[Pai] = []
         self.active: bool = False
         self.noDraw: bool = False
     def prepareNewRound(self):
-        self.tehai.clear()
-        self.fuuro.clear()
-        self.haiho.clear()
+        self.shoupai.clear()
+        self.fulu.clear()
+        self.paihe.clear()
     def drawHai(self) -> None:
-        self.tehai.append(hai)
+        hai = self.board.paishan.pop(0)
+        self.shoupai.append(hai)
     def turn(self) -> TAsync[bool]:
         if not self.noDraw:
             self.drawHai()
@@ -62,56 +62,56 @@ class BasePlayer(Generic[THai, TBoard]):
         return False
         yield
 
-class BaseBoard(Generic[TPlayer, THai]):
+class Board:
     PLAYER_NUM = 4
-    def __init__(self):
-        self.players: list[TPlayer] = [self.createPlayer(i) for i in range(self.PLAYER_NUM)]
-        self.allHai: list[THai] = self.prepareHai()
-        self.haiyama: list[THai] = []
-        self.wanpai: list[THai] = []
-        self.ba: int = 0
-        self.kyoku: int = 0
-        self.honba: int = 0
-        self.renchan: bool = False
+    def __init__(self, config: Config):
+        self.players: list[Player] = [self.createPlayer(i) for i in range(self.PLAYER_NUM)]
+        self.allPai: list[Pai] = self.preparePai()
+        self.paishan: list[Pai] = []
+        self.wangpai: list[Pai] = []
+        self.chang: int = 0
+        self.ju: int = 0
+        self.benchang: int = 0
+        self.lianzhuang: bool = False
         self.activePlayerPos: int = 0
-    def createPlayer(self, pos: int) -> TPlayer:
-        return TPlayer(pos)
-    def prepareHai(self) -> list[THai]:
-        hai = []
+    def createPlayer(self, pos: int) -> Player:
+        return Player(pos)
+    def preparePai(self) -> list[Pai]:
+        pai = []
         for color in Color:
             if color == Color.z:
                 continue
             for num in range(1, 10):
                 for _ in range(4):
-                    hai.append(BaseHai(color, num))
+                    pai.append(Pai(color, num))
         for num in range(1, 8):
             for _ in range(4):
-                hai.append(BaseHai(Color.z, num))
-        return hai
-    def shuffleHaiyama(self):
-        random.shuffle(self.haiyama)
+                pai.append(Pai(Color.z, num))
+        return pai
+    def shufflePaishan(self):
+        random.shuffle(self.paishan)
     @property
-    def toujyaPos(self) -> int:
-        return self.kyoku
+    def dongjiaPos(self) -> int:
+        return self.ju
     @property
-    def toujya(self) -> TPlayer:
-        return self.players[self.toujyaPos]
+    def dongjia(self) -> Player:
+        return self.players[self.dongjiaPos]
     @property
-    def activePlayer(self) -> TPlayer:
+    def activePlayer(self) -> Player:
         return self.players[self.activePlayerPos]
 
     def Game(self) -> TAsync[None]:
-        while self.ba < 4:
+        while self.chang < 4:
             yield from self.Round()
-            if not self.renchan:
-                self.honba = 0
-                self.kyoku += 1
-                if self.kyoku >= 4:
-                    self.ba += 1
-                    self.kyoku = 0
+            if not self.lianzhuang:
+                self.benchang = 0
+                self.ju += 1
+                if self.ju >= 4:
+                    self.chang += 1
+                    self.ju = 0
             else:
-                self.honba += 1
-                self.renchan = False
+                self.benchang += 1
+                self.lianzhuang = False
     def Round(self) -> TAsync[None]:
         self.RoundPrepareHaiyama()
         self.RoundPrepareWanpai()
@@ -119,30 +119,25 @@ class BaseBoard(Generic[TPlayer, THai]):
             player.prepareNewRound()
         self.RoundBeginHai()
         while True:
+            if len(self.paishan) == 0:
+                break
             ret = yield from self.activePlayer.turn()
             if ret:
                 break
     def RoundPrepareHaiyama(self):
-        self.haiyama = self.allHai.copy()
-        self.shuffleHaiyama()
+        self.paishan = self.allPai.copy()
+        self.shufflePaishan()
     def RoundPrepareWanpai(self):
-        self.wanpai = self.haiyama[-14:]
-        self.haiyama = self.haiyama[:-14]
+        self.wangpai = self.paishan[-14:]
+        self.paishan = self.paishan[:-14]
     def RoundBeginHai(self):
         for _ in range(3):
             for player in self.players:
                 for _ in range(4):
-                    player.tehai.append(self.haiyama.pop(0))
+                    player.shoupai.append(self.paishan.pop(0))
         for player in self.players:
-            player.tehai.append(self.haiyama.pop(0))
-        self.toujya.tehai.append(self.haiyama.pop(0))
-        self.toujya.active = True
-        self.toujya.noDraw = True
-        self.activePlayerPos = self.toujyaPos
-
-class TestHai(BaseHai):
-    pass
-class TestPlayer(BasePlayer[TestHai, 'TestBoard']):
-    pass
-class TestBoard(BaseBoard[TestPlayer, TestHai]):
-    pass
+            player.shoupai.append(self.paishan.pop(0))
+        self.dongjia.shoupai.append(self.paishan.pop(0))
+        self.dongjia.active = True
+        self.dongjia.noDraw = True
+        self.activePlayerPos = self.dongjiaPos
